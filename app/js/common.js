e871dce8aedc31d9d8a9db2c729839fc32e8d045
VitaliyNosov <webdeveloperstudio2017@gmail.com>
2020-03-04 13:24:41 +0200

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,19 +1,5 @@
 $(function() {
 
-	$("#tabs").tabs();
-
-
-
-
-	$('#li').hover(function () {
-		console.log('vfdvdvf');
-		$('#panel__menu').css("visibility : visible");
-	});
-
-
-
-	$(".popup").magnificPopup();
-
 	
 
 });
