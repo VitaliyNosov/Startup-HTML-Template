4ae31456b6fe11200b241fd4f9da12e037736989
Vitaliy <vitaliynosov2014@gmail.com>
2021-08-17 19:45:32 +0300

    first commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,34 @@
 $(function() {
 
-	
+    // button maps 
+
+    var header = document.getElementById("myDIV");
+    var btns = header.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
+
+
+    // mobile menu
+
+	$('#menu-triger').click(function () {
+
+		$('#mobile-menu').slideToggle(500);
+	});
+	$(window).resize(function () {
+
+		if ($(window).width() > 500) {
+
+			$('#mobile-menu').removeAttr('style');
+		}
+	});
+
+
+
 
 });
 
